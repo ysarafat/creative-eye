587696c189eb8e30eea66c727e54db974a1fbc27
Yeasir Arafat <ysarafat.bd@gmail.com>
2023-06-06 23:52:21 +0600

    create main layout

---
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div>
+            <h1 className="text-red-500">footer</h1>
+        </div>
+    );
+}
+
+export default Footer;
